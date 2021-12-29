e581f5eda58967c0afd228fa3fa7640645d3d517
Jeremy <jeremytbaxter@protonmail.com>
2021-12-29 20:31:46 +0000

    Changed colour scheme for the last time in a while

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel] = { "#eeeeee", "#0045ff" },
+	[SchemeOut] = { "#000000", "#0045ff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
